4c37e1a010e03c7d8b62b65462ee1b16ca18fa43
Lisa Caton <lisacaton01@gmail.com>
2018-04-16 11:29:19 -0400

    rooms rendered

--- src/App.js
+++ src/App.js
@@ -1,6 +1,21 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import * as firebase from 'firebase';
+import RoomList from './components/RoomList.js'
+
+
+    // Initialize Firebase
+    var config = {
+      apiKey: "AIzaSyA0GhwCA0e8fn36hEFCS2TL8tYACKKwhu0",
+      authDomain: "create-react-app-65812.firebaseapp.com",
+      databaseURL: "https://create-react-app-65812.firebaseio.com",
+      projectId: "create-react-app-65812",
+      storageBucket: "create-react-app-65812.appspot.com",
+      messagingSenderId: "1075541061306"
+    };
+    firebase.initializeApp(config);
+
 
 class App extends Component {
   render() {
@@ -10,9 +25,11 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="container">
+          <RoomList
+            firebase={firebase}
+            />
+        </div>
       </div>
     );
   }
