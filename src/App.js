3e7a665bf99095796543a2c3ed8e207cd93ffef4
Lisa Caton <lisacaton01@gmail.com>
2018-04-16 13:14:09 -0400

    live rooms updating

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">React Chat Room</h1>
         </header>
         <div className="container">
           <RoomList
--- src/components/RoomList.js
+++ src/components/RoomList.js
@@ -3,32 +3,45 @@ import React, { Component } from 'react';
 class RoomList extends Component {
   constructor(props) {
     super(props);
-
-    this.state = {
-      rooms: []
-    };
-
+    this.state = {rooms: [],name:''};
     this.roomsRef = this.props.firebase.database().ref('rooms');
-}
+    this.handleChange = this.handleChange.bind(this);
+    this.createRoom = this.createRoom.bind(this);
+  }
 
-    componentDidMount() {
-      this.roomsRef.on('child_added', snapshot => {
-        const room = snapshot.val();
-        room.key = snapshot.key;
-        this.setState({ rooms: this.state.rooms.concat( room ) });
-      });
-    }
+  componentDidMount() {
+    this.roomsRef.on('child_added', snapshot => {
+      const room = snapshot.val();
+      // room.key = snapshot.key;
+      this.setState({ rooms: this.state.rooms.concat( room ) });
+    });
+  }
+
+  createRoom(){
+    this.roomsRef.push({ name: this.state.name });
+    this.setState({ name: '' });
+  }
+
+  handleChange(e) {
+    this.setState({ name: e.target.value });
+  }
 
   render(){
+    const roomList = this.state.rooms.map((room) =>
+      <li key={room.key} onClick={(e) => this.selectRoom(room, e)}>{room.name}</li>
+      );
+
+    const roomForm = (
+      <form onSubmit = {this.createRoom}>
+        <input type="text" value={this.state.title} onChange={this.handleChange}/>
+        <input type="submit" value="Create"/>
+      </form>
+    );
+
     return(
       <div>
-        <ul>
-          {
-           this.state.rooms.map(function(room, i){
-             return <li key={i}>{room.name}</li>
-           })
-         }
-        </ul>
+        <ul>{roomList}</ul>
+        <div>{roomForm}</div>
       </div>
     );
   }
